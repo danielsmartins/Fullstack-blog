94e89ea5e9cee6a77ed1c7bf55deba2f82d5039b
Daniel Silva <danielsmartins19@gmail.com>
2025-07-13 15:38:35 -0300

    refatoração de rotas async/await & Testes

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,24 +1,67 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (req, res, next) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      res.json(blogs)
-    })
-    .catch(error => next(error))
+blogsRouter.get('/', async (req, res, next) => {
+  try {
+    const blogs = await Blog.find({})
+    res.json(blogs)
+  } catch (error) {
+    next(error)
+  }
 })
 
-blogsRouter.post('/', (req, res, next) => {
-  const blog = new Blog(req.body)
+blogsRouter.post('/', async (req, res, next) => {
+  const { title, author, url, likes } = req.body
 
-  blog
-    .save()
-    .then(result => {
-      res.status(201).json(result)
-    })
-    .catch(error => next(error))
+  if (!title || !url) {
+    return res.status(400).json({ error: 'title e url são obrigatórios' })
+  }
+
+  const blog = new Blog({
+    title,
+    author,
+    url,
+    likes
+  })
+
+  try {
+    const savedBlog = await blog.save()
+    res.status(201).json(savedBlog)
+  } catch (error) {
+    next(error)
+  }
 })
+
+blogsRouter.delete('/:id', async (req, res, next) => {
+  try {
+    await Blog.findByIdAndDelete(req.params.id)
+    res.status(204).end()
+  } catch (error) {
+    next(error)
+  }
+})
+
+blogsRouter.put('/:id', async (req, res, next) => {
+  const { likes } = req.body
+
+  try {
+    // Atualiza o campo likes e retorna o documento atualizado (new: true)
+    const updatedBlog = await Blog.findByIdAndUpdate(
+      req.params.id,
+      { likes },
+      { new: true, runValidators: true, context: 'query' }
+    )
+
+    if (updatedBlog) {
+      res.json(updatedBlog)
+    } else {
+      res.status(404).end()
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+
 
 module.exports = blogsRouter
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -13,14 +13,136 @@ beforeEach(async () => {
   await Blog.insertMany(helper.initialBlogs)
 })
 
-test.only('api get test', async () => {
-   const response = await api
-    .get('/api/blogs')
+describe('API tests', () => {
+
+  test('api get test', async () => {
+    const response = await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
+
+  test('verify id', async () => {
+    const response = await api.get('/api/blogs')
+    const blogs = response.body
+
+    blogs.forEach(blog => {
+      expect(blog.id).toBeDefined
+    })
+  })
+
+  test('verify post', async () => {
+    const blogBefore = await helper.blogsInDb()
+
+    const newBlog = {
+      title: 'Testando POST',
+      author: 'Daniel',
+      url: 'http://testepost.com',
+      likes: 7
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogAfter = await helper.blogsInDb()
+
+    expect(blogAfter).toHaveLength(blogBefore.length + 1)
+
+    const titulos = blogAfter.map(b => b.title)
+    expect(titulos).toContain('Testando POST')
+  })
+  test('default likes = 0', async () => {
+    const novoBlog = {
+      title: 'Sem likes',
+      author: 'Anônimo',
+      url: 'http://sem.likes.com'
+    }
+
+    const response = await api
+      .post('/api/blogs')
+      .send(novoBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    expect(response.body.likes).toBe(0)
+  })
+
+  test('verify missing title', async () => {
+    const novoBlog = {
+      author: 'Anônimo',
+      url: 'http://faltou-title.com',
+      likes: 3
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(novoBlog)
+      .expect(400)
+  })
+
+  test('verify missing url', async () => {
+    const novoBlog = {
+      author: 'Anônimo',
+      title: 'Faltando URL',
+      likes: 3
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(novoBlog)
+      .expect(400)
+  })
+
+  test('verify delete functionality', async () => {
+
+    const novoBlog = {
+      title: 'Blog a ser deletado',
+      author: 'Deletador',
+      url: 'http://delete.com',
+      likes: 1
+    }
+
+    const postResponse = await api
+      .post('/api/blogs')
+      .send(novoBlog)
+      .expect(201)
+
+    const idParaDeletar = postResponse.body.id
+
+    const blogsAntes = await helper.blogsInDb()
+
+    await api
+      .delete(`/api/blogs/${idParaDeletar}`)
+      .expect(204)
+
+    const blogsDepois = await helper.blogsInDb()
+    expect(blogsDepois).toHaveLength(blogsAntes.length - 1)
+
+    const ids = blogsDepois.map(b => b.id)
+    expect(ids).not.toContain(idParaDeletar)
+  })
+})
+
+test('verify update functionality', async () => {
+  const blogsAtuais = await helper.blogsInDb()
+  const blogParaAtualizar = blogsAtuais[0]
+
+  const novoLikes = blogParaAtualizar.likes + 10
+
+  const response = await api
+    .put(`/api/blogs/${blogParaAtualizar.id}`)
+    .send({ likes: novoLikes })
     .expect(200)
     .expect('Content-Type', /application\/json/)
-    
-     expect(response.body).toHaveLength(helper.initialBlogs.length)
+
+  expect(response.body.likes).toBe(novoLikes)
 })
+
 
 afterAll(async () => {
   await mongoose.connection.close()
